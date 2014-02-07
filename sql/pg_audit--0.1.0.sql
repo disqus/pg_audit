d6edef82f2007095b8a38833c9bd45f6faeb7091
David Fetter <david.fetter@disqus.com>
2014-02-07 13:58:40 -0800

    Moved the audit tables to their own schemas.

--- sql/pg_audit--0.1.0.sql
+++ sql/pg_audit--0.1.0.sql
@@ -3,35 +3,42 @@ RETURNS SETOF text
 IMMUTABLE STRICT
 LANGUAGE SQL
 AS $audit$
-WITH t0 AS (
+SELECT format('CREATE SCHEMA IF NOT EXISTS %I;', nspname || '_audit')
+FROM pg_catalog.pg_namespace n
+WHERE
+    n.nspname NOT IN ('pg_catalog','information_schema') AND /* Leave out the stuff in the catalog */
+    n.nspname !~ '(^(pg|)_|_audit$)'                         /* Also omit anything that looks like PostgreSQL, Slony or Audit owns it. */
+UNION ALL
+(
+    WITH t0 AS (
+        SELECT
+            n.nspname AS "schema",
+            c.relname AS "table",
+            a.attname AS "column_name",
+            pg_catalog.format_type(a.atttypid, a.atttypmod) AS "column_type"
+        FROM
+            pg_catalog.pg_attribute a
+        JOIN
+            pg_catalog.pg_class c
+            ON (
+                c.relkind = 'r' AND
+                c.oid = a.attrelid
+            )
+        JOIN
+            pg_catalog.pg_namespace n
+            ON (
+                c.relnamespace = n.oid AND
+                n.nspname NOT IN ('pg_catalog','information_schema') AND /* Leave out the stuff in the catalog */
+                n.nspname !~ '(^(pg|)_|_audit$)'                         /* Also omit anything that looks like PostgreSQL, Slony or Audit owns it. */
+            )
+        WHERE
+            a.attnum > 0 AND
+            NOT a.attisdropped
+        ORDER BY c.relname, a.attnum
+    )
     SELECT
-        quote_ident(n.nspname) AS "schema",
-        c.relname || '_audit' AS "table",
-        a.attname AS "column_name",
-        pg_catalog.format_type(a.atttypid, a.atttypmod) AS "column_type"
-    FROM
-        pg_catalog.pg_attribute a
-    JOIN
-        pg_catalog.pg_class c
-        ON (
-            c.relkind = 'r' AND
-            c.oid = a.attrelid
-        )
-    JOIN
-        pg_catalog.pg_namespace n
-        ON (
-            c.relnamespace = n.oid AND
-            n.nspname NOT IN ('pg_catalog','information_schema') AND /* Leave out the stuff in the catalog */
-            n.nspname !~ '(^(pg|)_|_audit$)'                         /* Also omit anything that looks like PostgreSQL, Slony or Audit owns it. */
-        )
-    WHERE
-        a.attnum > 0 AND
-        NOT a.attisdropped
-    ORDER BY c.relname, a.attnum
-)
-SELECT
-    format(
-        $q$CREATE TABLE %I.%I (
+        format(
+            $q$CREATE TABLE IF NOT EXISTS %I.%I (
     %s,
     stamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
     "current_user" TEXT NOT NULL DEFAULT current_user,
@@ -74,25 +81,27 @@ $$;
 CREATE TRIGGER %I
 AFTER INSERT OR UPDATE OR DELETE ON %I.%I
     FOR EACH ROW EXECUTE PROCEDURE %I.%I();$q$,
-        "schema", "table",
+        "schema" || '_audit', "table",
         string_agg(
             quote_ident("column_name" || '_old') || ' ' || column_type || E',\n    ' ||
             quote_ident("column_name" || '_new') || ' ' || column_type,   E',\n    '
         ),
+        "schema" || '_audit', "table",
         "schema", "table",
-        "schema", regexp_replace("table", '_audit$', ''),
-        "schema", regexp_replace("table", '_audit$', ''),
-        "schema", regexp_replace("table", '_audit$', ''),
         "schema", "table",
+        "schema", "table",
+        "schema" || '_audit', "table",
         string_agg(quote_ident("column_name" || '_old'), E',\n        '),
         string_agg(quote_ident("column_name" || '_new'), E',\n        '),
-        quote_ident("schema" || '_' || "table"),
-        "schema", regexp_replace("table", '_audit$', ''),
-        "schema", "table"
-    ) AS "table and trigger"
-FROM
-    t0
-GROUP BY "schema", "table"
+        quote_ident("schema" || '_' || "table" || '_audit'),
+        "schema", "table",
+        "schema" || '_audit', "table"
+
+        ) AS "table and trigger"
+        FROM
+            t0
+        GROUP BY "schema", "table"
+)
 UNION ALL
 /* Indexes for each unique key */
 (
@@ -106,7 +115,6 @@ UNION ALL
         JOIN
             pg_catalog.pg_namespace n
             ON (
-                c.relname !~ '_audit$' AND
                 c.relkind = 'r' AND
                 c.relnamespace = n.oid AND
                 n.nspname NOT IN ('pg_catalog','information_schema') AND
@@ -135,9 +143,9 @@ UNION ALL
     SELECT
         format(
             'CREATE INDEX %I ON %I.%I (%s);',
-            array_to_string(nspname || (relname || cols || v), '_'),
-            nspname,
-            relname || '_audit',
+            array_to_string(nspname || '_audit' || (relname || cols || v), '_'),
+            nspname || '_audit',
+            relname,
             (SELECT string_agg(u || '_' || v, ', ') FROM UNNEST(cols) AS u(u))
         )
     FROM
